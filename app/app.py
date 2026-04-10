6cb0e4514bdcbef493580f967092814b847edb41
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-04-10 17:08:33 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,3 +13,6 @@ def public():
 @app.route("/private")
 def private():
     return jsonify({"message": "Protected endpoint reached"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
